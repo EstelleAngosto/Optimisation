f79835b09a775c9f9d6387d89483d7f3c05d4ba2
Estelle ANGOSTO <estelle.angosto@gmail.com>
2022-01-10 17:51:34 +0100

    Modif code Newton-Raphson

--- newton.py
+++ newton.py
@@ -6,13 +6,26 @@ Created on Sun Jan  9 21:54:22 2022
 """
 
 def fonction(x): 
-    return(x) #Ecrire la fonction
+    return(x**3-x-1) #Ecrire la fonction
 
 def derive_fonction(x):
-    return(x) #Ecrire la fonction dérivée
+    return(3*x**2-1) #Ecrire la fonction dérivée
 
 def derive_seconde_fonction(x):
-    return(x) #Ecrire la dérivée seconde de la fonction
+    return(6*x) #Ecrire la dérivée seconde de la fonction
 
-first_point = 0.1
-epsilon = 0.01
+x = 1
+i = 0
+epsilon = 0.0001
+devfonc = derive_fonction(x)
+secdevfonc = derive_seconde_fonction(x)
+
+while abs(devfonc)>epsilon:
+    print ("Etape "+str(i))
+    print ("x : "+str(x))
+    x = x - devfonc/secdevfonc
+    devfonc = derive_fonction(x)
+    secdevfonc = derive_seconde_fonction(x)
+    i = i+1
+    
+print("Résultat final : "+str(x))
